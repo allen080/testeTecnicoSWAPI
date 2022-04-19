330dc7f3d06d7ad6ac93857f9721da46e07f726d
allen080 <luanfelliipe8@gmail.com>
2022-04-19 11:17:23 -0300

    Separando funcoes e comentarios do entendimento da API

---
+++ apiFunctions.py
@@ -0,0 +1,20 @@
+from math import floor
+
+timesDay = {'day':1,'week':7,'month':30,'year':360}
+
+def calculateShipDistance(shipMGLT, shipConsumables):
+    global timesDay
+    horas = -1
+
+    for i in timesDay:
+        if consumables.endswith(i):
+            horas = float(consumables.split(' ')[0]) * timesDay[i] * 24
+            break
+
+    if horas == -1:
+        print(f'[!] erro: {horas}')
+        exit(2)
+
+    paradaInicial = horas*shipMGLT
+    totalParadas = floor(1000000/paradaInicial)
+    return totalParadas
