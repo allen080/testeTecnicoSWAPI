90a7d3f1e437fd646eee6c8e8fc4c9c9d72ebc44
luan fellipe <luanfelliipe8@gmail.com>
2022-04-19 14:30:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,43 @@
 '''
-	Descrição:
-		Script de teste técnico para calculo do Nº de paradas necessárias para se
+	Descrição: Script de teste técnico para calculo do Nº de paradas necessárias para se
 		percorrer uma distancia nas espaçonaves listadas na SWAPI (https://swapi.dev)
-	Criado por:
-		Luan F. (allen080)
+	Criado por: Luan F. (allen080)
 '''
 
 import sys
-import requests
 
-sys.path.insert(1,"./pythonCodes")
-import swapiFunctions # arquivo com as funções utilizadas na API
+# Adiciona e importa o arquivo com as funções utilizadas na API
+sys.path.insert(1, "./pythonCodes")
+from swapiFunctions import calculateShipStops, getAllShipsData
 
 apiURL = f'https://swapi.dev/api/starships/' # URL Inicial da API
 
-try:
-	if len(sys.argv) < 2:
+try: # Pegar o valor da distancia total a ser percorrida e tenta converter pra float
+	if len(sys.argv) < 2: # Se não passou o valor como argumento de linha de comando, pede o valor na tela
 		totalDistance = input('Distância a ser percorrida (em MGLT): ')
-	else:
-		totalDistance = sys.argv[1]
-	totalDistance = float(totalDistance)
+	else: # Se passou o valor como argumento de linha de comando (Usado na mainGUI) 
+		totalDistance = sys.argv[1] 
+	
+	totalDistance = float(totalDistance) # tenta converter o valor recebido em float
 except ValueError:
 	print(f'\n[!] Distância "{totalDistance}" não é um número.')
 	sys.exit(1)
 except KeyboardInterrupt:
-	sys.exit(2)
+	sys.exit(3)
 
+allShipsInfo = getAllShipsData(apiURL)
 
-allShipsInfo = {}
+allShipStops = {} # Calculo da quantidade paradas necessarias por cada nave na distância informada
 
-while apiURL:
-	apiRequest = requests.get(apiURL)
-	if apiRequest.status_code != 200:
-		print(f'[!] {apiRequest.status_code}')
-		exit(1)
-
-	apiData = apiRequest.json() # Conteúdo da requisição em JSON
-	for shipInfo in apiData['results']:
-		shipName = shipInfo['name']
-		allShipsInfo[shipName] = []
-
-		if shipInfo['MGLT'].isnumeric():
-			allShipsInfo[shipName].append( float(shipInfo['MGLT']) )
-		else:
-			allShipsInfo[shipName].append('Unknown')
-		allShipsInfo[shipName].append( shipInfo['consumables'].lower().rstrip('s') )
-
-	apiURL = apiData['next']
-
-allShipStops = {}
-for shipInfo in allShipsInfo:	
-	shipStop = swapiFunctions.calculateShipStops(totalDistance, 
+for shipInfo in allShipsInfo: # Percorre cada nave obtida da API
+	# realiza o calculo do Nº de paradas em todas naves
+	shipStop = calculateShipStops(totalDistance, 
 		allShipsInfo[shipInfo][0], allShipsInfo[shipInfo][1]
 	)
 
-	allShipStops[shipInfo] = shipStop
+	allShipStops[shipInfo] = shipStop # adiciona nas informacoes totais
 
-for i in allShipStops:
+# exibe na tela o nome da nave e o tempo necessário
+for i in allShipStops: 
 	print(f'{i}: {allShipStops[i]}')
 
